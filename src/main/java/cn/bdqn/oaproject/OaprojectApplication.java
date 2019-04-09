0a9b7817b339931b562e22807d5d46858123e935
kathy <17636612897@163.com>
2019-04-09 21:01:07 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/oaproject/OaprojectApplication.java
@@ -0,0 +1,13 @@
+package cn.bdqn.oaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OaprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OaprojectApplication.class, args);
+    }
+
+}
