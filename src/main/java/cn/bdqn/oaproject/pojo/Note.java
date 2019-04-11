e63b502b5abf078cf6f5af826b8d9a3465251248
kathy <17636612897@163.com>
2019-04-11 11:22:38 +0800

    commit dao.

---
+++ src/main/java/cn/bdqn/oaproject/pojo/Note.java
@@ -0,0 +1,115 @@
+package cn.bdqn.oaproject.pojo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+/**
+ * 便签类
+ */
+@Entity
+@Table(name="NOTE")
+public class Note {
+
+    /**
+     * 便签编号
+     */
+    @Id
+    @Column(name="NOTE_ID")
+    private Integer noteId;
+
+    /**
+     * 便签标题
+     */
+    @Column(name="NOTE_TITLE")
+    private String noteTitle;
+
+    /**
+     * 便签内容
+     */
+    @Column(name="CONTENT")
+    private String content;
+
+    /**
+     * 创建者
+     */
+    @Column(name="CREATOR")
+    private Integer creator;
+
+    /**
+     * 创建日期
+     */
+    @Column(name="CREATION_DATE")
+    private Date creationDate;
+
+    /**
+     * 修改者
+     */
+    @Column(name="MENDER")
+    private Integer mender;
+
+    /**
+     * 修改日期
+     */
+    @Column(name="MODIFY_DATE")
+    private Date modifyDate;
+
+
+    public Integer getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(Integer noteId) {
+        this.noteId = noteId;
+    }
+
+    public String getNoteTitle() {
+        return noteTitle;
+    }
+
+    public void setNoteTitle(String noteTitle) {
+        this.noteTitle = noteTitle;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Integer creator) {
+        this.creator = creator;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Integer getMender() {
+        return mender;
+    }
+
+    public void setMender(Integer mender) {
+        this.mender = mender;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+}
