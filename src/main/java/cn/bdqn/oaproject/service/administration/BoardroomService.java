340be8971b1f08b5c9dd192741b8dcd4b7b31759
kathy <17636612897@163.com>
2019-04-11 22:00:46 +0800

    April 11st commit boardroom function.

--- src/main/java/cn/bdqn/oaproject/service/administration/BoardroomService.java
+++ src/main/java/cn/bdqn/oaproject/service/administration/BoardroomService.java
@@ -1,7 +1,43 @@
 package cn.bdqn.oaproject.service.administration;
 
+import cn.bdqn.oaproject.pojo.Boardroom;
+
+import java.util.List;
+
 /**
  * 会议室业务操作接口
  */
 public interface BoardroomService {
+
+    /**
+     *  获取所有会议室
+     * @return 会议室列表
+     */
+    List<Boardroom> findAllBoardroom()throws  Exception;
+
+    /**
+     * 新增会议室
+     * @param boardroom 会议室对象
+     * @return
+     */
+    boolean addNewBoardroom(Boardroom boardroom)throws  Exception;
+
+    /**
+     * 修改会议室
+     * @param boardroom
+     * @return
+     */
+    boolean updateBoardroom(Boardroom boardroom)throws  Exception;
+
+    /**
+     * 删除会议室
+     * @param id
+     * @return
+     */
+    boolean delBoardroom(Integer id)throws  Exception;
+
+
+
+
+
 }
