382f1d0d5915aab66f47afdfc4a82a01a2550abf
毛鹏辉 <1084207275@qq.com>
2019-04-23 11:40:19 +0800

    commit MyDesktop

---
+++ src/main/java/cn/bdqn/oaproject/utils/WaitingTaskQueryUtil.java
@@ -0,0 +1,31 @@
+package cn.bdqn.oaproject.utils;
+
+/**
+ * 待处理任务查询工具类
+ */
+public class WaitingTaskQueryUtil {
+
+    /**
+     * 根据任务类型Id以及申请记录Id查询当前待处理任务的详细信息
+     * @param typeId
+     * @param applyId
+     * @return
+     */
+    public Object getInfoByTypeIdAndApplyId(Integer typeId,Integer applyId){
+        Object obj=null;
+        switch (typeId){
+            case 1:
+
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+        }
+        return obj;
+    }
+}
